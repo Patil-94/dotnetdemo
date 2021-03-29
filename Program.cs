4ff57a5700110bc9c60731db283b221494eb4cc1
Patil-94 <ankitakhairnar10@gmail.com>
2021-03-29 12:34:07 +0530

    Add Helloworld Project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    /// <summary>
+    /// Tested this sample appliaction for printing hello message
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// main method is the start point for console application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloworld");
+            Console.Read();   
+        }
+    }
+}
